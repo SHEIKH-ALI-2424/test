fa2b6dcbc188bca08c42dfbcda19cf34efb3a3f7
SHEIKH-ALI-2424 <sheikhali2406@gmail.com>
2025-08-08 15:31:06 +0500

    Update main-updater.js

--- plugins/main-updater.js
+++ plugins/main-updater.js
@@ -19,7 +19,7 @@ cmd({
         await reply("🔍 Checking for SHEIKH-ALI-MD updates...");
 
         // Fetch the latest commit hash from GitHub
-        const { data: commitData } = await axios.get("https://api.github.com/repos/SHEIKH-ALI-2424/SHEIKH-ALI-MD/commits/main");
+        const { data: commitData } = await axios.get("https://api.github.com/repos/SHEIKH-ALI-2424/test/commits/main");
         const latestCommitHash = commitData.sha;
 
         // Get the stored commit hash from the database
@@ -33,7 +33,7 @@ cmd({
 
         // Download the latest code
         const zipPath = path.join(__dirname, "latest.zip");
-        const { data: zipData } = await axios.get("https://github.com/SHEIKH-ALI-2424/SHEIKH-ALI-MD/archive/main.zip", { responseType: "arraybuffer" });
+        const { data: zipData } = await axios.get("https://github.com/SHEIKH-ALI-2424/test/archive/main.zip", { responseType: "arraybuffer" });
         fs.writeFileSync(zipPath, zipData);
 
         // Extract ZIP file
@@ -44,7 +44,7 @@ cmd({
 
         // Copy updated files, preserving config.js and app.json
         await reply("🔄 Replacing files...");
-        const sourcePath = path.join(extractPath, "SHEIKH-ALI-MD-main");
+        const sourcePath = path.join(extractPath, "test-main");
         const destinationPath = path.join(__dirname, '..');
         copyFolderSync(sourcePath, destinationPath);
 
